9d3e036be393a8c80594c881920739d1e5cd0a81
tumit <tumit@live.com>
2024-09-26 21:28:25 +0700

    add card.service

--- src/mobile/card.service.spec.ts
+++ src/mobile/card.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CardService } from './card.service';
+import { FormatService } from './format.service';
 
 describe('CardService', () => {
   let service: CardService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CardService],
+      providers: [CardService, FormatService],
     }).compile();
 
     service = module.get<CardService>(CardService);
@@ -15,4 +16,15 @@ describe('CardService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should show nameCard', () => {
+    // * arrange
+    const name = 'John'
+    const mobileNo = '0891234567'
+    // * act
+    const result = service.nameCard(name, mobileNo)
+    // * assert
+    expect(result).toEqual('John: Tel. 089-123-4567')
+  })
+
 });
--- src/mobile/card.service.ts
+++ src/mobile/card.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { FormatService } from './format.service';
 
 @Injectable()
-export class CardService {}
+export class CardService {
+  
+  constructor(private formatService: FormatService) {}
+
+  nameCard(name: string, mobileNo: string): string {
+      return `${name}: ${this.formatService.mobileFormat(mobileNo)}`
+  }
+}
+

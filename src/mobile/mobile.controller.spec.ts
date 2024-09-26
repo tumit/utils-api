4f889a130f5aa975b7bb9cad4317c64ce8c094cd
tumit <tumit@live.com>
2024-09-26 21:59:05 +0700

    fix mobile.controller test

--- src/mobile/mobile.controller.spec.ts
+++ src/mobile/mobile.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MobileController } from './mobile.controller';
+import { CardService } from './card.service';
+import { FormatService } from './format.service';
 
 describe('MobileController', () => {
   let controller: MobileController;
@@ -7,6 +9,7 @@ describe('MobileController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MobileController],
+      providers: [CardService, FormatService]
     }).compile();
 
     controller = module.get<MobileController>(MobileController);

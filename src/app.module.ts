318081d2cca212f2b686249f786ce3918126a21f
tumit <tumit@live.com>
2024-09-26 20:41:25 +0700

    add mobile module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FormatController } from './format/format.controller';
+import { MobileModule } from './mobile/mobile.module';
 
 @Module({
-  imports: [],
+  imports: [MobileModule],
   controllers: [AppController, FormatController],
   providers: [AppService],
 })
